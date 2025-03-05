5c8b3629356cce9ebd8840c88f5dd89fbdb8e90f
martinss27 <leoalmeidamartins@gmail.com>
2025-03-05 16:28:47 -0300

    Now the user can choose any image from their PC!

--- image_resizer.py
+++ image_resizer.py
@@ -1,16 +1,23 @@
 from PIL import Image
+import os
 
 
-def image_resize(size1,size2):
-    image = Image.open("python-logo.jpg")
+def image_resize(width, height, input_path):
+    if not os.path.exists(input_path):
+        print(f"Error: File '{input_path}' not found.")
+        return
 
-    print(f'Current size: {image.size}')
+    try:
+        image = Image.open(input_path)
+        print(f"Current size: {image.size}")
 
-    resized_image = image.resize((size1,size2))
+        resized_image = image.resize((width, height))
 
-    resized_image.save("python-logo-resized"+ str(size1) +".jpg")
+        file_name, ext = os.path.splitext(os.path.basename(input_path))
+        output_path = f"{file_name}-resized-{width}x{height}{ext}"
 
-size1 = int(input("Enter Widht: "))
-size2 = int(input("Enter Length: "))
+        resized_image.save(output_path)
+        print(f"Resized image saved ass '{output_path}'")
 
-image_resize(size1, size2)
+    except Exception as e:
+        print(f"Error processing image: {e}")
